8f3e42a4ef9e236e4e941bc50ac8323841c257a5
muhammadsubhan23 <subhanboxer720@gmail.com>
2024-07-06 01:15:33 -0700

    Add files via upload

---
+++ assigment/assigmentt.py
@@ -0,0 +1,24 @@
+#  FIRST QUESTION 
+
+a = 4
+b = 5
+print("Area Of Triangle")
+print(0.5*a*b)
+
+
+#    SECOND QUESTION
+
+radius = 6
+pie    = 2.16
+print("Area Of Circle")
+print(pie*radius*radius)
+
+
+#   THIRD   QUESTION
+
+s = 9
+print("Area of Square ")
+print(s*s)
+
+name= (input("ENTER YOUR NAME"))
+print(name)
